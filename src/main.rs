83dd188c09070db5974124d20df8bc8c4ebeeaf8
ldu1020 <ldu1020@gmail.com>
2022-01-14 23:25:19 +0900

    feat: guess game example

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
+use std::io;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    println!("Guess the number!");
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); 
+
+    io::stdin() 
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
-    
